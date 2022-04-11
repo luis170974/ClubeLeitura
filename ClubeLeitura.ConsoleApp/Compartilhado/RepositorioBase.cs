eaa157c9979a218d8b9187a47595d9dd5731b9e2
LUIS HENRIQUE FERREIRA KRAUS <luishenriquekraus@hotmail.com>
2022-04-11 11:16:19 -0300

    Add project files.

---
+++ ClubeLeitura.ConsoleApp/Compartilhado/RepositorioBase.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace ClubeLeitura.ConsoleApp.Compartilhado
+{
+    public abstract class RepositorioBase<T> where T : EntidadeBase
+    {
+        protected readonly List<T> registros;
+
+        protected int contadorNumero;
+
+        public RepositorioBase()
+        {
+            registros = new List<T>();
+        }
+
+        public virtual string Inserir(T entidade)
+        {
+            entidade.numero = ++contadorNumero;
+
+            registros.Add(entidade);
+
+            return "REGISTRO_VALIDO";
+        }
+
+        public void Editar(int numeroSelecionado, T entidade)
+        {
+
+            for (int i = 0; i < registros.Count; i++)
+            {
+                if (registros[i].numero == numeroSelecionado)
+                {
+                    entidade.numero = numeroSelecionado;
+                    registros[i] = entidade;
+
+                    break;
+                }
+            }
+        }
+
+        public bool Excluir(int numeroSelecionado)
+        {
+            T entidadeSelecionada = SelecionarRegistro(numeroSelecionado);
+
+            if (entidadeSelecionada == null)
+                return false;
+
+            registros.Remove(entidadeSelecionada);
+
+            return true;
+        }
+
+        public T SelecionarRegistro(int numeroRegistro)
+        {
+            return registros.Find(x => x.numero.Equals(numeroRegistro));
+          
+        
+        }
+
+        public List<T> SelecionarTodos()
+        {
+            return registros;
+        }
+
+        public bool ExisteRegistro(int numeroRegistro)
+        {
+            foreach (T registro in registros)
+                if (registro.numero == numeroRegistro)
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ ClubeLeitura.ConsoleApp/ModuloReserva/RepositorioReserva.cs
@@ -0,0 +1,34 @@
+﻿using ClubeLeitura.ConsoleApp.Compartilhado;
+using System.Collections.Generic;
+
+namespace ClubeLeitura.ConsoleApp.ModuloReserva
+{
+    public class RepositorioReserva : RepositorioBase<Reserva>
+    {
+        public RepositorioReserva()
+        {
+        }
+
+        public override string Inserir(Reserva reserva)
+        {
+            reserva.numero = ++contadorNumero;
+
+            reserva.Abrir();
+
+            registros.Add(reserva);
+
+            return "REGISTRO_VALIDO";
+        }
+
+        public Reserva[] SelecionarReservasEmAberto()
+        {
+            List<Reserva> reservasInseridas = new List<Reserva>();
+
+            foreach (Reserva reserva in registros)
+                if (reserva.estaAberta)
+                    reservasInseridas.Add(reserva);
+
+            return reservasInseridas.ToArray();
+        }
+    }
+}
